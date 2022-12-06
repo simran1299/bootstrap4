86829ab60a64ac34bcd01c4758a00a0534ce002f
simran1299 <simrandesai1299@gmail.com>
2022-12-06 16:07:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bootstarp4(request):
+    return render(request,'bootstarp4.html')
+
+def boot_dn(request):
+    return render(request,'boot_dn.html')
